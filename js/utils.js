2100a0909101a0b5005911a0024f3636be8565f5
liisauosukainen <liisa.uosukainen@saimia.fi>
2021-04-09 09:29:07 +0300

    js file updated

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,15 @@
+window.onload = function () {
+    init();
+};
+
+function init() {
+    console.log('Page loaded and init-function executed.');
+}
+
 function getDate() {
     var today = new Date();
-    var now = today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
-    var infoText = "Tänään on " + now;
+    var now = today.getDate() + '.' + (today.getMonth() + 1) + '.' + today.getFullYear();
+    var infoText = 'Tänään on ' + now;
     alert(infoText);
     document.getElementById('date-info').innerHTML = infoText;
     console.log(infoText);
